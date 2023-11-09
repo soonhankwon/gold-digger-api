7b60d1685b48a10e6da54ba8af0fec02f3462233
soonhankwon <soonable@gmail.com>
2023-11-09 23:40:50 +0900

    Initial commit

---
+++ src/test/java/dev/golddiggerapi/GoldDiggerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.golddiggerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldDiggerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
