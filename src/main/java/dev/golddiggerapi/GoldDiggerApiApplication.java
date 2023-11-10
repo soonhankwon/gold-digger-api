c5c70cbd76fdc8f2d499e134be85d2ddf23a2b19
soonhankwon <soonable@gmail.com>
2023-11-10 12:40:25 +0900

    feat: Security 임시 중지(#3)

--- src/main/java/dev/golddiggerapi/GoldDiggerApiApplication.java
+++ src/main/java/dev/golddiggerapi/GoldDiggerApiApplication.java
@@ -2,8 +2,9 @@ package dev.golddiggerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class GoldDiggerApiApplication {
 
     public static void main(String[] args) {
