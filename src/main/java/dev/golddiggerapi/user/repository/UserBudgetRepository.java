9e2e54230defbf0e84ad86f204bbd3b7e4b41cd5
soonhankwon <soonable@gmail.com>
2023-11-10 14:54:12 +0900

    feat: 기존 동일한 카테고리의 예산설정이 있는지 확인(#7)

--- src/main/java/dev/golddiggerapi/user/repository/UserBudgetRepository.java
+++ src/main/java/dev/golddiggerapi/user/repository/UserBudgetRepository.java
@@ -1,9 +1,12 @@
 package dev.golddiggerapi.user.repository;
 
+import dev.golddiggerapi.expenditure.domain.ExpenditureCategory;
+import dev.golddiggerapi.user.domain.User;
 import dev.golddiggerapi.user.domain.UserBudget;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserBudgetRepository extends JpaRepository<UserBudget, Long> {
+    boolean existsByUserAndExpenditureCategory(User user, ExpenditureCategory category);
 }
