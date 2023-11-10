aa26e80781a611b7a06bef6eda35162923b26b32
soonhankwon <soonable@gmail.com>
2023-11-10 12:42:58 +0900

    feat: 유저 생성 및 DB 저장 로직 - 서비스 레이어(#3)

---
+++ src/main/java/dev/golddiggerapi/user/service/UserService.java
@@ -0,0 +1,34 @@
+package dev.golddiggerapi.user.service;
+
+import dev.golddiggerapi.user.controller.dto.UserSignupRequest;
+import dev.golddiggerapi.user.domain.User;
+import dev.golddiggerapi.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public String createUser(UserSignupRequest request) {
+        if(isExistsAccountName(request.accountName())) {
+            throw new IllegalArgumentException("exist account name in db");
+        }
+        Function<String, String> encodeFunction =
+                password -> new BCryptPasswordEncoder().encode(password);
+        User user = new User(request, encodeFunction);
+        userRepository.save(user);
+        return "created";
+    }
+
+    private boolean isExistsAccountName(String accountName) {
+        return userRepository.existsByAccountName(accountName);
+    }
+}
