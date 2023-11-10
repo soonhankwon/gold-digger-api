554c6e479ae1274f9db3d3eec0b5742643761eea
soonhankwon <soonable@gmail.com>
2023-11-10 11:30:05 +0900

    feat: 지출 도메인 & 레포지토리(#1)

---
+++ src/main/java/dev/golddiggerapi/expenditure/domain/Expenditure.java
@@ -0,0 +1,34 @@
+package dev.golddiggerapi.expenditure.domain;
+
+import dev.golddiggerapi.user.domain.User;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Entity
+@Table(name = "expenditure")
+public class Expenditure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer amount;
+
+    @Enumerated(EnumType.STRING)
+    private ExpenditureStatus expenditureStatus;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "expenditure_category_id")
+    private ExpenditureCategory expenditureCategory;
+}
