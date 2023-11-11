ff7507b5e1cef6e28fdcec53ae201bcc65cc41c6
soonhankwon <soonable@gmail.com>
2023-11-11 23:02:25 +0900

    feat: 카테고리별 평균 비율 통계 메서드(#9)

--- src/main/java/dev/golddiggerapi/expenditure/repository/ExpenditureRepositoryCustom.java
+++ src/main/java/dev/golddiggerapi/expenditure/repository/ExpenditureRepositoryCustom.java
@@ -4,13 +4,19 @@ import dev.golddiggerapi.expenditure.controller.dto.ExpenditureByUserRequest;
 import dev.golddiggerapi.expenditure.controller.dto.ExpenditureCategoryAndAmountResponse;
 import dev.golddiggerapi.expenditure.controller.dto.ExpenditureMemoAndAmountResponse;
 import dev.golddiggerapi.expenditure.controller.dto.ExpenditureMinAndMax;
+import dev.golddiggerapi.expenditure.controller.dto.UserExpenditureAvgRatioByCategoryStatisticResponse;
 import dev.golddiggerapi.user.domain.User;
 
 import java.util.List;
 
 public interface ExpenditureRepositoryCustom {
     List<ExpenditureCategoryAndAmountResponse> statisticExpenditureCategoryAndAmount(User user, ExpenditureByUserRequest request);
+
     List<ExpenditureMemoAndAmountResponse> getExpendituresMemoAndAmountByCondition(User user, ExpenditureByUserRequest request);
+
     ExpenditureMinAndMax getExpenditureMinAndMaxByUser(User user, ExpenditureByUserRequest request);
+
     Long getExpendituresSumByUserAndCondition(User user, ExpenditureByUserRequest request);
+
+    List<UserExpenditureAvgRatioByCategoryStatisticResponse> statisticAvgRatioByCategory();
 }
