47a98bf59117680d962ce5b10b3523306f90ffe0
soonhankwon <soonable@gmail.com>
2023-11-11 23:07:33 +0900

    feat: 유저 지출카테고리 one to many & 영속성 전이 / 지출 카테고리 추가 도메인 로직(#9)

--- src/main/java/dev/golddiggerapi/user/domain/User.java
+++ src/main/java/dev/golddiggerapi/user/domain/User.java
@@ -4,6 +4,8 @@ import dev.golddiggerapi.user.controller.dto.UserSignupRequest;
 import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
 
 @NoArgsConstructor
@@ -19,8 +21,25 @@ public class User {
 
     private String password;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<UserExpenditureCategory> userExpenditureCategories = new ArrayList<>();
+
     public User(UserSignupRequest request, Function<String, String> encoderFunction) {
         this.accountName = request.accountName();
         this.password = encoderFunction.apply(request.password());
+    }
+
+    public void addExpenditureCategory(UserExpenditureCategory userExpenditureCategory) {
+        if (this.userExpenditureCategories.contains(userExpenditureCategory)) {
+            UserExpenditureCategory userCategory =
+                    this.userExpenditureCategories
+                            .stream()
+                            .filter(i -> i.equals(userExpenditureCategory))
+                            .findFirst()
+                            .orElseThrow();
+            userCategory.updateAmount(userExpenditureCategory.getAmount());
+            return;
+        }
+        this.userExpenditureCategories.add(userExpenditureCategory);
     }
 }
