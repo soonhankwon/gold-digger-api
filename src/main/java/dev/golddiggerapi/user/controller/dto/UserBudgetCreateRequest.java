9ee9dca9d44efb08f73af74a3a3ed45f31727116
soonhankwon <soonable@gmail.com>
2023-11-12 12:50:45 +0900

    feat: 유저예산 생성 DTO - 필드 추가 및 유효성 검사(#9)

--- src/main/java/dev/golddiggerapi/user/controller/dto/UserBudgetCreateRequest.java
+++ src/main/java/dev/golddiggerapi/user/controller/dto/UserBudgetCreateRequest.java
@@ -1,9 +1,27 @@
 package dev.golddiggerapi.user.controller.dto;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+
+import java.time.Year;
 
 public record UserBudgetCreateRequest(
         @Min(value = 0)
-        Long amount
+        Long amount,
+
+        Integer year,
+
+        @Min(1) @Max(12)
+        Integer month
 ) {
+    public UserBudgetCreateRequest(@Min(value = 0) Long amount,
+                                   Integer year,
+                                   @Min(1) @Max(12) Integer month) {
+        this.amount = amount;
+        if (year < Year.now().getValue()) {
+            throw new IllegalArgumentException("year can't before now year");
+        }
+        this.year = year;
+        this.month = month;
+    }
 }
