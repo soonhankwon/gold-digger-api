55d7bf1dcc3304bed964d1b95b7a9110803d31c0
soonhankwon <soonable@gmail.com>
2023-11-13 00:11:41 +0900

    feat: 유저의 이달 설정예산 전체리스트 조회 / 유저의 이달의 예산 & 카테고리ID로 조회 메서드(#15)

--- src/main/java/dev/golddiggerapi/user/repository/UserBudgetRepository.java
+++ src/main/java/dev/golddiggerapi/user/repository/UserBudgetRepository.java
@@ -7,8 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserBudgetRepository extends JpaRepository<UserBudget, Long>, UserBudgetRepositoryCustom {
     boolean existsByUserAndExpenditureCategoryAndPlannedMonth(User user, ExpenditureCategory category, LocalDateTime plannedMonth);
+    List<UserBudget> findUserBudgetsByUserAndPlannedMonth(User user, LocalDateTime plannedMonth);
+    Optional<UserBudget> findUserBudgetByUserAndExpenditureCategory_IdAndPlannedMonth(User user, Long categoryId, LocalDateTime plannedMonth);
 }
