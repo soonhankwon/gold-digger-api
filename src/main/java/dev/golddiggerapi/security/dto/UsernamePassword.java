b28f2152d34d4f99f6d18743b32d44dcc8d1f3f6
soonhankwon <soonable@gmail.com>
2023-11-14 10:30:30 +0900

    feat: username, password DTO(#21)

---
+++ src/main/java/dev/golddiggerapi/security/dto/UsernamePassword.java
@@ -0,0 +1,16 @@
+package dev.golddiggerapi.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsernamePassword {
+
+    private String username;
+    private String password;
+}
