5e185adb232105ed80d7fe062e5e7005f6ee7fce
soonhankwon <soonable@gmail.com>
2023-11-14 23:14:27 +0900

    feat: 지출 카테고리별 지출금액 응답 - 스웨거 문서화(#27)

--- src/main/java/dev/golddiggerapi/expenditure/controller/dto/ExpenditureCategoryAndAmountResponse.java
+++ src/main/java/dev/golddiggerapi/expenditure/controller/dto/ExpenditureCategoryAndAmountResponse.java
@@ -1,10 +1,18 @@
 package dev.golddiggerapi.expenditure.controller.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "지출 카테고리별 지출금액 응답 DTO")
 public record ExpenditureCategoryAndAmountResponse(
+
+        @Schema(description = "카테고리 ID", example = "1")
         Long categoryId,
+
+        @Schema(description = "카테고리 이름", example = "식비")
         String name,
+
+        @Schema(description = "카테고리 지출 총합", example = "50000")
         Long sum
 ) {
 
