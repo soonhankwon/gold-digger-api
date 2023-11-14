4d553d88f9d6ad9465e9e9b8254ec065a6297fe4
soonhankwon <soonable@gmail.com>
2023-11-14 23:14:41 +0900

    feat: 지출 카테고리 API - 스웨거 문서화(#27)

--- src/main/java/dev/golddiggerapi/expenditure/controller/ExpenditureCategoryController.java
+++ src/main/java/dev/golddiggerapi/expenditure/controller/ExpenditureCategoryController.java
@@ -2,6 +2,8 @@ package dev.golddiggerapi.expenditure.controller;
 
 import dev.golddiggerapi.expenditure.controller.dto.ExpenditureCategoryResponse;
 import dev.golddiggerapi.expenditure.service.ExpenditureCategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/expenditures/categories")
 @RequiredArgsConstructor
+@RequestMapping("/api/expenditures/categories")
+@Tag(name = "지출 카테고리 API")
 public class ExpenditureCategoryController {
 
     private final ExpenditureCategoryService expenditureCategoryService;
 
+    @Operation(summary = "지출 카테고리 목록 조회 API")
     @GetMapping
     public ResponseEntity<List<ExpenditureCategoryResponse>> getExpenditureCategories() {
         List<ExpenditureCategoryResponse> res =
