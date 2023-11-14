464fda5ffb50cb5ecca79cf1e660c5651254d2e4
soonhankwon <soonable@gmail.com>
2023-11-14 23:14:55 +0900

    feat: 지출 카테고리 응답 - 스웨거 문서화(#27)

--- src/main/java/dev/golddiggerapi/expenditure/controller/dto/ExpenditureCategoryResponse.java
+++ src/main/java/dev/golddiggerapi/expenditure/controller/dto/ExpenditureCategoryResponse.java
@@ -1,9 +1,15 @@
 package dev.golddiggerapi.expenditure.controller.dto;
 
 import dev.golddiggerapi.expenditure.domain.ExpenditureCategory;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(name = "지출 카테고리 응답 DTO")
 public record ExpenditureCategoryResponse(
+
+        @Schema(description = "카테고리 ID", example = "1")
         Long id,
+
+        @Schema(description = "카테고리 이름", example = "식비")
         String name
 ) {
     public static ExpenditureCategoryResponse toResponse(ExpenditureCategory category) {
