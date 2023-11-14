288ec5b23beb05fb1928d85f32374de04cdfbeac
soonhankwon <soonable@gmail.com>
2023-11-14 17:23:48 +0900

    feat: 지출분석 이벤트 record(#22)

---
+++ src/main/java/dev/golddiggerapi/notification/event/ExpenditureAnalyzeEvent.java
@@ -0,0 +1,8 @@
+package dev.golddiggerapi.notification.event;
+
+import dev.golddiggerapi.expenditure.controller.dto.ExpenditureByTodayResponse;
+
+public record ExpenditureAnalyzeEvent(
+        ExpenditureByTodayResponse expenditureByTodayResponse
+) {
+}
