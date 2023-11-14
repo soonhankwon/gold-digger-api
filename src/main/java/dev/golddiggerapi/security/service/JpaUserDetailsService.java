ef23bf819737cca57c5c4209d57b5f53622e74e9
soonhankwon <soonable@gmail.com>
2023-11-14 11:07:05 +0900

    fear: jpa 유저디테일 서비스 - UserDetailsService(시큐리티)구현(#21)

---
+++ src/main/java/dev/golddiggerapi/security/service/JpaUserDetailsService.java
@@ -0,0 +1,24 @@
+package dev.golddiggerapi.security.service;
+
+import dev.golddiggerapi.security.principal.UserPrincipal;
+import dev.golddiggerapi.user.domain.User;
+import dev.golddiggerapi.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+        return new UserPrincipal(user);
+    }
+}
