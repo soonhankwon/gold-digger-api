f8ba986715cd6d3e66c5f00946f377af0f57b9de
soonhankwon <soonable@gmail.com>
2023-11-14 23:13:06 +0900

    feat: 오늘 지출 안내 및 분석 응답 - 스웨거 문서화(#27)

--- src/main/java/dev/golddiggerapi/expenditure/controller/dto/ExpenditureByTodayResponse.java
+++ src/main/java/dev/golddiggerapi/expenditure/controller/dto/ExpenditureByTodayResponse.java
@@ -1,10 +1,18 @@
 package dev.golddiggerapi.expenditure.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
+@Schema(description = "오늘 지출 안내 및 분석 응답 DTO")
 public record ExpenditureByTodayResponse(
+
+        @Schema(description = "오늘 지출 총액", example = "47200")
         Long expenditureSum,
+
+        @Schema(description = "오늘 기준 사용 적절한 지출 금액", example = "42000")
         Long reasonableExpenditureSum,
+
         List<ExpenditureByTodayByCategoryStatisticsResponse> expenditureByTodayByCategoryStatisticsResponses
 ) {
 }
