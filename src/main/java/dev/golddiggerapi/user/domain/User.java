8399974242bd31cea2482d60adf9d787df09821a
soonhankwon <soonable@gmail.com>
2023-11-15 10:54:34 +0900

    feat: 주요 쿼리 where 절 사용 컬럼 인덱스 생성(#32)

--- src/main/java/dev/golddiggerapi/user/domain/User.java
+++ src/main/java/dev/golddiggerapi/user/domain/User.java
@@ -10,17 +10,22 @@ import java.util.function.Function;
 @NoArgsConstructor
 @Getter
 @Entity
-@Table(name = "`user`")
+@Table(name = "`user`", indexes = {
+        @Index(name = "idx_username_idx", columnList = "username"),
+        @Index(name = "idx_subscribe_notification_idx", columnList = "subscribe_notification")
+})
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "username")
     private String username;
 
     private String password;
 
+    @Column(name = "subscribe_notification")
     private Boolean subscribeNotification;
 
     public User(UserSignupRequest request, Function<String, String> encoderFunction) {
