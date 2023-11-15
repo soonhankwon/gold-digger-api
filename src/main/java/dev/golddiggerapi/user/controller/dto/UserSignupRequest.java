b3c99edc84fdca437490c64c92195c7e18e7c081
soonhankwon <soonable@gmail.com>
2023-11-15 18:21:42 +0900

    feat: 게정명 6자이상 30자이하, 패스워드 8글자 이상, 숫자, 문자, 특수문자 포함 유효성 검사

--- src/main/java/dev/golddiggerapi/user/controller/dto/UserSignupRequest.java
+++ src/main/java/dev/golddiggerapi/user/controller/dto/UserSignupRequest.java
@@ -2,16 +2,24 @@ package dev.golddiggerapi.user.controller.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @Schema(description = "회원 가입 요청 DTO")
 public record UserSignupRequest(
 
         @NotBlank(message = "계정명은 하나 이상의 공백이 아닌 문자를 포함해야 합니다.")
-        @Schema(description = "계정명", example = "abc")
+        @Size(min = 6, max = 30, message = "계정명은 6자이상 30자이하로 작성해야합니다.")
+        @Schema(description = "계정명", example = "username")
         String username,
 
         @NotBlank(message = "패스워드는 하나 이상의 공백이 아닌 문자를 포함해야 합니다.")
-        @Schema(description = "패스워드", example = "1234")
+        @Size(min = 8, message = "패스워드는 최소 8글자 이상이어야 합니다.")
+        @Pattern(
+                regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&+=!]).*$",
+                message = "패스워드는 숫자, 문자, 특수 문자를 포함해야 합니다."
+        )
+        @Schema(description = "패스워드", example = "password1!")
         String password,
 
         @NotBlank(message = "알람수신여부는 true 또는 false 여야 합니다.")
