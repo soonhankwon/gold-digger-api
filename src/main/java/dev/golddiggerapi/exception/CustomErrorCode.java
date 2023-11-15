7db108c4993658e6dcaab50c4c6b839bfbfc4e38
soonhankwon <soonable@gmail.com>
2023-11-15 23:26:00 +0900

    feat: 커스텀 에러코드 정의

--- src/main/java/dev/golddiggerapi/exception/CustomErrorCode.java
+++ src/main/java/dev/golddiggerapi/exception/CustomErrorCode.java
@@ -8,13 +8,15 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum CustomErrorCode {
 
-    ACCESS_TOKEN_EXPIRED("AU01", HttpStatus.UNAUTHORIZED, "Access token expired!"),
-    INVALID_TOKEN("AU02", HttpStatus.UNAUTHORIZED, "Invalid Token!"),
+    ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "Access token expired!"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid Token!"),
+    INFORMATION_NOT_MATCHED(HttpStatus.CONFLICT, "Information not matched!"),
+    LOGIN_REQUIRED_FIRST(HttpStatus.UNAUTHORIZED, "Login required first"),
+    USERNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "Username already exists"),
+    USER_NOT_FOUND_DB(HttpStatus.NOT_FOUND, "User not found in db"),
+    CATEGORY_NOT_FOUND_DB(HttpStatus.NOT_FOUND, "Category not found in db"),
+    NOT_USER_EXPENDITURE_ID(HttpStatus.BAD_REQUEST, "This is not user's expenditure id");
 
-    INFORMATION_NOT_MATCHED("AU03", HttpStatus.CONFLICT, "Information not matched!"),
-    LOGIN_REQUIRED_FIRST("AU04", HttpStatus.UNAUTHORIZED, "Login required first");
-
-    private final String errorCode;
 
     private final HttpStatus status;
 
