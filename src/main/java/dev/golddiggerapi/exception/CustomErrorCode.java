9d27ac3beba911cbd5c26cc21bb82bad22911dff
soonhankwon <soonable@gmail.com>
2023-11-16 11:22:09 +0900

    feat: 유저예산이 DB에 없을 경우 에러코드

--- src/main/java/dev/golddiggerapi/exception/CustomErrorCode.java
+++ src/main/java/dev/golddiggerapi/exception/CustomErrorCode.java
@@ -9,14 +9,15 @@ import org.springframework.http.HttpStatus;
 public enum CustomErrorCode {
 
     ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "Access token expired!"),
+    CATEGORY_NOT_FOUND_DB(HttpStatus.NOT_FOUND, "Category not found in db"),
+    DUPLICATED_USER_BUDGET(HttpStatus.CONFLICT, "Duplicated user budget exists"),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid Token!"),
     INFORMATION_NOT_MATCHED(HttpStatus.CONFLICT, "Information not matched!"),
     LOGIN_REQUIRED_FIRST(HttpStatus.UNAUTHORIZED, "Login required first"),
+    NOT_USER_EXPENDITURE_ID(HttpStatus.BAD_REQUEST, "This is not user's expenditure id"),
     USERNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "Username already exists"),
     USER_NOT_FOUND_DB(HttpStatus.NOT_FOUND, "User not found in db"),
-    CATEGORY_NOT_FOUND_DB(HttpStatus.NOT_FOUND, "Category not found in db"),
-    NOT_USER_EXPENDITURE_ID(HttpStatus.BAD_REQUEST, "This is not user's expenditure id"),
-    DUPLICATED_USER_BUDGET(HttpStatus.CONFLICT, "Duplicated user budget exists");
+    USER_BUDGET_NOT_FOUND_DB(HttpStatus.NOT_FOUND, "User budget not found in db");
 
 
     private final HttpStatus status;
