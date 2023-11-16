845e5ee153ab89c86f66355fc1ecfe86e9c5efbb
soonhankwon <soonable@gmail.com>
2023-11-16 10:35:12 +0900

    feat: 카테고리 목록 조회 결과 캐싱 - 553ms -> 20ms(#36)

--- src/main/java/dev/golddiggerapi/expenditure/service/ExpenditureCategoryService.java
+++ src/main/java/dev/golddiggerapi/expenditure/service/ExpenditureCategoryService.java
@@ -4,6 +4,7 @@ import dev.golddiggerapi.expenditure.controller.dto.ExpenditureCategoryResponse;
 import dev.golddiggerapi.expenditure.domain.ExpenditureCategory;
 import dev.golddiggerapi.expenditure.repository.ExpenditureCategoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public class ExpenditureCategoryService {
 
     private final ExpenditureCategoryRepository expenditureCategoryRepository;
 
+    @Cacheable(value = "expenditure:category:1:collections", cacheManager = "cacheManager")
     public List<ExpenditureCategoryResponse> getExpenditureCategories() {
         List<ExpenditureCategory> categories = expenditureCategoryRepository.findAll();
 
