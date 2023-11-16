05da133a780c3c8b873f656a3a49fb3999d313c9
soonhankwon <soonable@gmail.com>
2023-11-16 11:16:11 +0900

    feat: 중복된 유저예산이 있을경우 에러코드

--- src/main/java/dev/golddiggerapi/exception/CustomErrorCode.java
+++ src/main/java/dev/golddiggerapi/exception/CustomErrorCode.java
@@ -15,7 +15,8 @@ public enum CustomErrorCode {
     USERNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "Username already exists"),
     USER_NOT_FOUND_DB(HttpStatus.NOT_FOUND, "User not found in db"),
     CATEGORY_NOT_FOUND_DB(HttpStatus.NOT_FOUND, "Category not found in db"),
-    NOT_USER_EXPENDITURE_ID(HttpStatus.BAD_REQUEST, "This is not user's expenditure id");
+    NOT_USER_EXPENDITURE_ID(HttpStatus.BAD_REQUEST, "This is not user's expenditure id"),
+    DUPLICATED_USER_BUDGET(HttpStatus.CONFLICT, "Duplicated user budget exists");
 
 
     private final HttpStatus status;
