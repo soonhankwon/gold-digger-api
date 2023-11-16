a84f56255c8747b70a50b367e69aac9458338cf9
soonhankwon <soonable@gmail.com>
2023-11-16 18:46:44 +0900

    feat: 유저가입시 요청 Validation

--- src/main/java/dev/golddiggerapi/user/controller/UserController.java
+++ src/main/java/dev/golddiggerapi/user/controller/UserController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,7 @@ public class UserController {
 
     @Operation(summary = "회원가입 API")
     @PostMapping("/signup")
-    public ResponseEntity<String> createUser(@RequestBody UserSignupRequest request) {
+    public ResponseEntity<String> createUser(@Validated @RequestBody UserSignupRequest request) {
         String res = userService.createUser(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(res);
     }
