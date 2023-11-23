2858601b5c2ca90fc1a987771e58a9a3932bc4cc
soonhankwon <soonable@gmail.com>
2023-11-23 17:09:43 +0900

    feat: 지출 서비스 핸들러 - 의존성을 관리 & 락 책임, 필요한 함수를 지출 서비스에 전달(#47)

--- src/main/java/dev/golddiggerapi/expenditure/service/ExpenditureServiceHandler.java
+++ src/main/java/dev/golddiggerapi/expenditure/service/ExpenditureServiceHandler.java
@@ -2,6 +2,8 @@ package dev.golddiggerapi.expenditure.service;
 
 import dev.golddiggerapi.budget_consulting.service.BudgetConsultingService;
 import dev.golddiggerapi.expenditure.controller.dto.ExpenditureByTodayRecommendationResponse;
+import dev.golddiggerapi.expenditure.controller.dto.ExpenditureRequest;
+import dev.golddiggerapi.global.util.strategy.RedissonLockContext;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,26 +12,35 @@ public class ExpenditureServiceHandler {
 
     private final BudgetConsultingService budgetConsultingService;
     private final ExpenditureService expenditureService;
+    private final RedissonLockContext redissonLockContext;
 
     public ExpenditureServiceHandler(final BudgetConsultingService budgetConsultingService,
-                                     final ExpenditureService expenditureService) {
+                                     final ExpenditureService expenditureService,
+                                     final RedissonLockContext redissonLockContext) {
         this.budgetConsultingService = budgetConsultingService;
         this.expenditureService = expenditureService;
+        this.redissonLockContext = redissonLockContext;
+    }
+
+    public String createExpenditure(String username, Long categoryId, ExpenditureRequest request) {
+        redissonLockContext.executeLock(username,
+                () -> expenditureService.createExpenditure(username, categoryId, request));
+        return "created";
     }
 
     @Scheduled(cron = "0 0 8 * * *")
     public void sendExpenditureRecommendationByToday() {
         expenditureService.sendExpenditureRecommendationByToday(
                 budgetConsultingService::analyzeBudgetStatus,
-                budgetConsultingService::getMinimumAvailableExpenditureV1
+                budgetConsultingService::getMinimumAvailableExpenditure
         );
     }
 
     public ExpenditureByTodayRecommendationResponse getExpenditureRecommendationByToday(String username) {
-        return expenditureService.getExpenditureRecommendationByTodayV1(
+        return expenditureService.getExpenditureRecommendationByToday(
                 username,
                 budgetConsultingService::analyzeBudgetStatus,
-                budgetConsultingService::getMinimumAvailableExpenditureV1
+                budgetConsultingService::getMinimumAvailableExpenditure
         );
     }
 }
