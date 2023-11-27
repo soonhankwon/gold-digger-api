d6367e45f1e49f1f2ebc68abd8a7236339e69243
soonhankwon <soonable@gmail.com>
2023-11-27 21:48:56 +0900

    feat: 데이터 기반으로 카테고리별 최소 하루 지출금액 산출 - 비즈니스 로직(#51)

--- src/main/java/dev/golddiggerapi/budget_consulting/service/BudgetConsultingService.java
+++ src/main/java/dev/golddiggerapi/budget_consulting/service/BudgetConsultingService.java
@@ -1,26 +1,29 @@
 package dev.golddiggerapi.budget_consulting.service;
 
+import dev.golddiggerapi.expenditure.domain.ExpenditureCategory;
+import dev.golddiggerapi.expenditure.repository.ExpenditureCategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BudgetConsultingService {
+
+    private final ExpenditureCategoryRepository expenditureCategoryRepository;
 
     //TODO 고도화 필요
     public String analyzeBudgetStatus(Long realAvailableExpenditure) {
-        if(realAvailableExpenditure < 0) {
+        if (realAvailableExpenditure < 0) {
             return "예산이 초과한 상태입니다. 쫌더 아껴서 쓰세요! 화이팅";
         }
-        if(realAvailableExpenditure > 100_000L) {
+        if (realAvailableExpenditure > 100_000L) {
             return "절약을 잘 실천하고 계세요! 오늘도 절약 도전!";
         }
         return "쫌 더 화이팅하면 골드를 캘 수 있습니다. 화이팅!";
     }
 
-    //TODO 데이터 통계 기반으로 고도화 필요
     public Long getMinimumAvailableExpenditure(String categoryName) {
-        if(categoryName.equals("식비")) {
-            return 15_000L;
-        }
-        return 10_000L;
+        ExpenditureCategory expenditureCategory = expenditureCategoryRepository.findExpenditureCategoryByName(categoryName);
+        return Long.valueOf(expenditureCategory.getMinimumExpenditurePerDay());
     }
 }
