759c74ba0ccb27f74139df3dbc4f7db700bd94bc
soonhankwon <soonable@gmail.com>
2023-11-27 21:50:58 +0900

    feat: csv파일 조회 & 카테고리 별 최소비용 계산 및 저장로직(#51)

--- src/main/java/dev/golddiggerapi/expenditure/service/ExpenditureCategoryService.java
+++ src/main/java/dev/golddiggerapi/expenditure/service/ExpenditureCategoryService.java
@@ -4,15 +4,27 @@ import dev.golddiggerapi.expenditure.controller.dto.ExpenditureCategoryResponse;
 import dev.golddiggerapi.expenditure.domain.ExpenditureCategory;
 import dev.golddiggerapi.expenditure.repository.ExpenditureCategoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ExpenditureCategoryService {
+
+    @Value("${category-expenditure.file-path}")
+    private String filePath;
 
     private final ExpenditureCategoryRepository expenditureCategoryRepository;
 
@@ -23,5 +35,28 @@ public class ExpenditureCategoryService {
         return categories.stream()
                 .map(ExpenditureCategoryResponse::toResponse)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void saveMinimumExpenditureByCategoryFromCsv() {
+        Map<String, Integer> map = new ConcurrentHashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String row;
+            while ((row = br.readLine()) != null) {
+                String[] split = row.split(",");
+                String name = split[1];
+
+                int expenditureAveragePerMonth = Integer.parseInt(split[2]);
+                int expenditureAveragePerDay = expenditureAveragePerMonth / 30;
+                map.put(name, map.getOrDefault(name, 0) + expenditureAveragePerDay);
+            }
+            map.keySet()
+                    .forEach(i -> {
+                        ExpenditureCategory expenditureCategory = expenditureCategoryRepository.findExpenditureCategoryByName(i);
+                        expenditureCategory.updateMinimumExpenditurePerDay(map.get(i));
+                    });
+        } catch (IOException e) {
+            log.error("csv file invalid={}", e.getMessage());
+        }
     }
 }
